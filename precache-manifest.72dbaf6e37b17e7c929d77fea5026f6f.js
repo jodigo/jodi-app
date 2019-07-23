77bc3e4e4025fdee84247acf796365f764fcc90e
jodigo <jgunawan.1998@gmail.com>
2019-07-22 19:36:18 -0700

    Updates

---
+++ precache-manifest.72dbaf6e37b17e7c929d77fea5026f6f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cd39448921de878ac33622c0c61db58",
+    "url": "/static/media/Lars-Mono.0cd39448.woff2"
+  },
+  {
+    "revision": "3a5318dbaa2579f63327",
+    "url": "/static/css/main.97b52b44.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "d1dbdfc89ca4b31540c2091f3c25e310",
+    "url": "/static/media/pp.d1dbdfc8.png"
+  },
+  {
+    "revision": "ffa71a17748bec01ea15",
+    "url": "/static/js/2.ffa71a17.chunk.js"
+  },
+  {
+    "revision": "fe257c4d18a550acb04e2333efc3c689",
+    "url": "/static/media/Lars-Light.fe257c4d.woff2"
+  },
+  {
+    "revision": "3a5318dbaa2579f63327",
+    "url": "/static/js/main.3a5318db.chunk.js"
+  },
+  {
+    "revision": "703f6f95453b67cfd952c9da9c2d32a5",
+    "url": "/static/media/Lars-Mono.703f6f95.eot"
+  },
+  {
+    "revision": "eccb076b71ad3d7f73ce3341e7d6f034",
+    "url": "/static/media/Lars-Mono.eccb076b.woff"
+  },
+  {
+    "revision": "17b75bb45a3dbbed4414b41083d253ba",
+    "url": "/static/media/Lars-Light.17b75bb4.eot"
+  },
+  {
+    "revision": "7577ad03a88ee4ac5fcdaf7e6ee1300c",
+    "url": "/static/media/Lars-Light.7577ad03.woff"
+  },
+  {
+    "revision": "ffa71a17748bec01ea15",
+    "url": "/static/css/2.61eeff29.chunk.css"
+  },
+  {
+    "revision": "e2222ef73bc0cda0f899f399b02381d6",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/jodi-app/precache-manifest.fc98c060f06da54c2e801defa9c1b20b.js"
+  "/precache-manifest.72dbaf6e37b17e7c929d77fea5026f6f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/jodi-app/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
