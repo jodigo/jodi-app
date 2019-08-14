520fe6158bafe0beaf6908a00cbf477517bdf3cb
jodigo <jgunawan.1998@gmail.com>
2019-08-14 12:05:04 -0700

    Remove cruft

--- src/App.js
+++ src/App.js
@@ -16,17 +16,14 @@ export default function App() {
     const isReturningUser = "dark" in localStorage;
     const savedMode = JSON.parse(localStorage.getItem("dark"));
     const userPrefersDark = getPrefColorScheme();
-    // if mode was saved --> dark / light
+    
     if (isReturningUser) {
       return savedMode;
-      // if preferred color scheme is dark --> dark
     } else if (userPrefersDark) {
       return true;
-      // otherwise --> light
     } else {
       return false;
     }
-    // return savedMode || false;
   }
 
   function getPrefColorScheme() {
