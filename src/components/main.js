d1768549b87d4b8f4fc5d956eb411fed63f09fd5
jodigo <jgunawan.1998@gmail.com>
2019-09-04 12:47:47 -0700

    WIP: Transition page effect

--- src/components/main.js
+++ src/components/main.js
@@ -5,14 +5,23 @@ import HomePage from './homepage';
 import About from './about';
 import Feed from './feed';
 import Contact from './contact';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const Main = () => (
-  <Switch>
-    <Route exact path="/" component={HomePage} />
-    <Route path="/about" component={About} />
-    <Route path="/feed" component={Feed} />
-    <Route path="/contact" component={Contact} />
-  </Switch>
-  
+  <Route render={({location}) => (
+    <TransitionGroup>
+      <CSSTransition 
+        key={location.key}
+        timeout={500}
+        classNames="fade">
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/about" component={About} />
+          <Route path="/feed" component={Feed} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </CSSTransition>
+    </TransitionGroup>
+  )}/>
 );
 export default Main;
